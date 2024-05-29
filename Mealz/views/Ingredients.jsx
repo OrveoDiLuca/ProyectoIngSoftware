ca6651b9ee1d579eeaf76a49c70cf0eb8b922981
Emilgon <97489051+Emilgon@users.noreply.github.com>
2024-05-28 22:46:40 -0400

    cambios Mealz 28/05/2024
    se agregó la función de agregar ingredientes al apretar el botón de agregar

--- Mealz/views/Ingredients.jsx
+++ Mealz/views/Ingredients.jsx
@@ -1,30 +1,31 @@
-import { Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, ScrollView, View } from 'react-native';
 import { Icon } from 'react-native-elements';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CustomButton from '../components/CustomButton';
-
-
 
 export function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredient = ({}) => {
-    const newIngredient = <CustomButton/>
-    setIngredients([...ingredients, newIngredient]);
-  }
+  const addIngredient = () => {
+    const ingredientNumber = ingredients.length + 1; // Get the next ingredient number
+    setIngredients([...ingredients, <View style={styles.ingredientContainer} key={ingredients.length}>
+      <Text style={{ marginBottom: 10 }}>Ingrediente {ingredientNumber}</Text>
+      {/* Add input fields or other ingredient details here */}
+    </View>]);
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.add}> Agregar Ingredientes </Text>
       <TouchableOpacity onPress={addIngredient} style={styles.button}>
-          <Icon type = 'material-community' name = 'plus-circle' color = 'white' size= {35} />
+        <Icon type='material-community' name='plus-circle' color='white' size={35} />
       </TouchableOpacity>
-      
+      {ingredients.map((ingredient, index) => (
+        <View key={index}>{ingredient}</View>
+      ))}
     </ScrollView>
-  )
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -41,5 +42,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 22.5,
     fontFamily: 'Inter-ExtraBold',
-  }
+  },
+  ingredientContainer: {
+    margin: 20,
+    alignItems: 'center',
+    backgroundColor: '#00ffa6',
+    padding: 30,
+    borderRadius: 40,
+  },
 });
+
+
