59ef9b4c583beb2dbc078ad254407089ab16079f
alexialeon2104 <a.leon2020@correo.unimet.edu.ve>
2024-06-29 20:20:54 -0400

    Informacion de recetas completada

--- Mealz/views/RecipeInfo.jsx
+++ Mealz/views/RecipeInfo.jsx
@@ -1,42 +1,45 @@
 import React from "react";
-import { View, Image, Text, useWindowDimensions,Button } from "react-native";
+import { View, Image, Text, useWindowDimensions,Button, StyleSheet, Linking  } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { ScrollView } from "react-native-gesture-handler";
 import { useEffect, useState} from "react";
 import HTML from 'react-native-render-html';
 import { set } from "firebase/database";
+import { useFonts } from 'expo-font';
+
 
 
 //Vista de la información de la receta
 
 export function RecipeInfo() { 
 
+    let [fontsLoaded] = useFonts({
+        'PlaywriteNGModern': require('../assets/fonts/PlaywriteNGModern-Regular.ttf'),
+      });
+    
+     
+
     const { params } = useRoute();
     const navigation = useNavigation();
     let recipeInfo = params.recipeInfo; 
 
  const windowWidth = useWindowDimensions().width;
+ const tagsStyles = {body: {
+    fontFamily: 'PlaywriteNGModern',
+    textAlign: 'justify', // Justify text alignment
+    fontSize: 17, // Larger text size
+    lineHeight: 25,
+    marginTop: 15,
+   
+  },
+  p: {
+    textAlign: 'justify', // Justify text alignment for paragraphs specifically
+    fontSize: 18, // Larger text size for paragraphs
+    fontFamily: 'PlaywriteNGModern'
+  }}
 
-  //const [instructions, setInstructions] = useState({parsedInstructions: []});
 
 
-// Fetch and set the recipe instructions
-// const handleInstructions = async () => {
-//     try {
-//         const response = await fetch(`https://api.spoonacular.com/recipes/${recipeInfo.id}/analyzedInstructions?apiKey=2a3a5f802ec24072beae5c801581794f`);
-//         const data = await response.json();
-//         setInstructions(data);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// };
-
-// Call handleInstructions on component mount
-// useEffect(() => {
-//     handleInstructions();
-// }, []);
-
-    
     return (
         
         //Bloque en blanco de la carta
@@ -47,20 +50,41 @@ export function RecipeInfo() {
                     source={{ uri: recipeInfo.image }}/> 
 
                 <View className="px-3 pb-4 space-y-4"> 
-                    <Text className="text-lg text-justify text-lg font-semibold pt-3">{recipeInfo.title}</Text>  
-                    <Text className="text-lg text-justify text-lg font-semibold pt-3">Summary:</Text>
-                    <HTML source={{ html: recipeInfo.summary }} contentWidth={windowWidth}/>
-                    <Text className="text-lg text-justify text-lg font-semibold pt-3">Ingredients:</Text> 
+                    <Text className="text-lg text-justify text-lg" style={styles.titleStyle}>{recipeInfo.title}</Text>  
+                    <Text className="text-lg text-justify text-lg font-semibold pt-3" style={{fontFamily: 'PlaywriteNGModern', color: '#297045', fontSize: 24,}}>Summary:</Text>
+                    <HTML source={{ html: recipeInfo.summary }} contentWidth={windowWidth} tagsStyles={tagsStyles}/>
+                    <Text className="text-lg text-justify text-lg font-semibold pt-3" style={{fontFamily: 'PlaywriteNGModern', color: '#297045', fontSize: 24,}}>Ingredients:</Text> 
+                    {recipeInfo.extendedIngredients.map((ingredient) => (
+                    <Text className="text-lg text-justify text-lg font-semibold pt-3" style={{fontFamily: 'PlaywriteNGModern', color: '#297045' }}>
+                    {ingredient.original}
+                    </Text>
+))}
 
-                    <Text className="text-lg text-justify text-lg font-semibold pt-3">Steps:</Text>  
-                    {/* {instructions.steps.map((step) => (
-                    // <Text className="text-lg text-justify text-lg font-semibold pt-3">
-                    //   Step {step.number} : {step.step} 
-                    // </Text>
-                    ))} */}
+
+                      
+                    <Text
+                        className="text-lg text-justify text-lg font-semibold pt-3"
+                        onPress={() => Linking.openURL(recipeInfo.sourceUrl)}
+                        style={{ fontFamily: 'PlaywriteNGModern', color: '#297045', fontSize: 24, textDecorationLine: 'underline' }}
+>
+                        Get complete recipe on {recipeInfo.sourceName}'s website to check steps and more!
+                        </Text>
+                    
                 </View> 
             </ScrollView>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    titleStyle: {
+      fontFamily: 'PlaywriteNGModern',
+      fontSize: 24,
+      textAlign: 'justify',
+      fontWeight: 'bold',
+      color: '#297045',
+      lineHeight: 38,
+      marginTop: 20,
+    }
+  });
 
